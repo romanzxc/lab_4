9ee29be274e71a62f7e55f2eb23b10c2b6c11466
asumibus <102917338+romanzxc@users.noreply.github.com>
2023-10-02 21:40:22 +0300

    Add files via upload

---
+++ Laba4_V_6/ComplexNumber.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+
+namespace Laba4_V_6
+{
+    class ComplexNumber
+    {
+        private double _realPart;
+        private double _imaginaryPart;
+
+        private double RealPart
+        {
+            get => _realPart;
+            set => _realPart = value;
+        }
+        private double ImaginaryPart
+        {
+            get => _imaginaryPart;
+            set => _imaginaryPart = value;
+        }
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        /// <param name="realPart">действительная часть комплексного числа</param>
+        /// <param name="imaginaryPart">мнимая часть комплексного числа</param>
+        public ComplexNumber(double realPart, double imaginaryPart)
+        {
+            RealPart = realPart;
+            ImaginaryPart = imaginaryPart;
+        }
+        /// <summary>
+        /// модуль комплексного числа
+        /// </summary>
+        /// <returns></returns>
+        public double Module()
+        {
+            return Math.Sqrt(RealPart * RealPart + ImaginaryPart * ImaginaryPart);
+        }
+        /// <summary>
+        /// угол фи комплексного числа 
+        /// </summary>
+        /// <returns></returns>
+        private double AnglePhi()
+        {
+            return Math.Atan2(ImaginaryPart, RealPart);
+        }
+        /// <summary>
+        /// возведение в степень комплексного числа
+        /// </summary>
+        /// <param name="n">степень возведения </param>
+        /// <returns></returns>
+        public ComplexNumber Pow(int n)
+        {
+            if (n > 0)
+            {
+                double newMagnitude = Math.Pow(this.Module(), n);
+                return new ComplexNumber(newMagnitude * Math.Cos(this.AnglePhi() * n), newMagnitude * Math.Sin(this.AnglePhi() * n));
+            }
+            else if (n == 1)
+            {
+                return new ComplexNumber(1, 0);
+            }
+            else
+            {
+                double newMagnitude = Math.Pow(this.Module(), 1 / Math.Abs(n));
+                return new ComplexNumber(Math.Cos(this.AnglePhi() / n) * newMagnitude, Math.Sin(this.AnglePhi() / n) * newMagnitude);
+            }
+        }
+        /// <summary>
+        /// метод вывода комплексного числа
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $" {RealPart} + {ImaginaryPart}i";
+        }
+  
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+        {
+            if (c1 is  null || c2 is  null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                return new ComplexNumber(c1.RealPart + c2.RealPart, c1.ImaginaryPart + c2.ImaginaryPart);
+            }
+
+        }
+
+        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
+        {
+            if (c1 is null || c2 is null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                return c1 + new ComplexNumber(-c2.RealPart, -c2.ImaginaryPart);
+            }
+        }
+        public static ComplexNumber operator *(ComplexNumber c1, ComplexNumber c2)
+        {
+            if (c1 is  null || c2 is  null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                return new ComplexNumber(c1.RealPart * c2.RealPart - c1.ImaginaryPart * c2.ImaginaryPart, c1.RealPart * c2.ImaginaryPart + c1.ImaginaryPart * c2.RealPart);
+            }
+        }
+        public static ComplexNumber operator /(ComplexNumber c1, ComplexNumber c2)
+        {
+            if (c1 is null || c2 is  null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                double denominator = c2.RealPart * c2.RealPart + c2.ImaginaryPart * c2.ImaginaryPart;
+                if (denominator != 0)
+                {
+                    double realPart = (c1.RealPart * c2.RealPart + c1.ImaginaryPart * c2.ImaginaryPart) / denominator;
+                    double imaginaryPart = (c1.ImaginaryPart * c2.RealPart - c1.RealPart * c2.ImaginaryPart) / denominator;
+                    return new ComplexNumber(realPart, imaginaryPart);
+                }
+                else
+                {
+                    throw new DivideByZeroException();
+                }
+            }
+        }
+    }
+}
---
+++ Laba4_V_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Laba4_V_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber a1 = new ComplexNumber(9.32, 2.4 );
+            ComplexNumber a2 = new ComplexNumber(-45, -0.44);
+            ComplexNumber a3 = new ComplexNumber(0.007, 0);
+
+            Console.WriteLine("a1= " + a1);
+            Console.WriteLine("a2= " + a2);
+            Console.WriteLine("a3= " + a3);
+            ComplexNumber x = a2.Pow(4)+  ( a1 - a2) / (a3* a1);
+            Console.WriteLine("x=a2^4 + ( a1 - a2) / (a3* a1)");
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("Модуль x="+x.Module());
+
+        }
+    }
+}
